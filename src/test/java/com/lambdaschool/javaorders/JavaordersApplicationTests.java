3174b63a490ac7117ccff79a8cb08945581ee9b9
Patrick Martin <patrickmartin88@gmail.com>
2019-02-19 14:22:44 -0800

    Initial Commit

---
+++ src/test/java/com/lambdaschool/javaorders/JavaordersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lambdaschool.javaorders;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaordersApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
