c37e264a8bb53068232f52bf6b10992c782dae0b
Patrick Martin <patrickmartin88@gmail.com>
2019-02-20 12:54:07 -0800

    Completed Models

--- src/main/java/com/lambdaschool/javaorders/Models/Order.java
+++ src/main/java/com/lambdaschool/javaorders/Models/Order.java
@@ -1,4 +1,32 @@
 package com.lambdaschool.javaorders.Models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="orders")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long ordernum;
+
+    private double ordamount, advanceamount;
+    private String orddescription;
+
+    @ManyToOne
+    @JoinColumn(name ="custcode", nullable = false)
+    @JsonIgnore
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "agentcode", nullable = false)
+    @JsonIgnore
+    private Agent agent;
+
+    public Order() {
+    }
 }
